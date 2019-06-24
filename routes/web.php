b7ab6384c5b9167db7644088bd0a25b3649b3af7
wanwb <409329702@qq.com>
2019-06-24 03:53:55 +0000

    用户界面 - 商品详情页

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,8 @@
 */
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
-
+Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 Auth::routes(['verify' => true]);
 
 
