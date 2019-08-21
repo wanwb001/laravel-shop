36d22f1ac6714390d93b071be3f6f26ab1d1c1fd
wanwb <409329702@qq.com>
2019-08-21 03:12:56 +0000

    管理后台 - 同意退款 - 微信支付退款

--- routes/web.php
+++ routes/web.php
@@ -41,11 +41,12 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('orders/{order}/review', 'OrdersController@review')->name('orders.review.show');
     Route::post('orders/{order}/review', 'OrdersController@sendReview')->name('orders.review.store');
     Route::post('orders/{order}/apply_refund', 'OrdersController@applyRefund')->name('orders.apply_refund');
+
 });
 
 Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
 Route::post('payment/wechat/notify', 'PaymentController@wechatNotify')->name('payment.wechat.notify');
-
+Route::post('payment/wechat/refund_notify', 'PaymentController@wechatRefundNotify')->name('payment.wechat.refund_notify');
 
 Route::get('/sendSMS', function() {
     $testService = resolve('TestService');
